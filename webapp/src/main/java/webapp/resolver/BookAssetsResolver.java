b2bf12f6f25bc92d50f09d5b6d266704b7d1e328
Davide Botti <davidebotti92@gmail.com>
2025-06-10 21:43:29 +0200

    Adds the assets to the Book type

---
+++ webapp/src/main/java/webapp/resolver/BookAssetsResolver.java
@@ -0,0 +1,22 @@
+package webapp.resolver;
+
+import model.Asset;
+import model.Book;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BookAssetsResolver {
+
+    public BookAssetsResolver() {
+    }
+
+    @SchemaMapping(typeName = "Book", field = "assets")
+    public List<Asset> assets(Book book) {
+        return List.of(new Asset("image", "http://example.com"), new Asset("video", "http://example2.com"));
+    }
+}
+
+
