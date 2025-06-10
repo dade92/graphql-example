71aaaf5a8a8ccd0c743f0eb4f8e9295e95bcf99b
dbotti <davide.botti@lastminute.com>
2025-06-10 16:56:50 +0200

    Tests refactor

--- webapp/src/test/kotlin/utils/FixtureLoader.kt
+++ webapp/src/test/kotlin/utils/FixtureLoader.kt
@@ -1,18 +1,8 @@
 package utils
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.nio.charset.StandardCharsets
 
 object FixtureLoader {
-    val objectMapper: ObjectMapper =
-        jacksonObjectMapper()
-            .findAndRegisterModules()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-
     @JvmStatic
     fun readFile(path: String): String = this.javaClass.getResource(path)!!.readText(StandardCharsets.UTF_8)
 }
