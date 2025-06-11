04a5d40164a1faba06c948155db1281e6c702816
dbotti <davide.botti@lastminute.com>
2025-06-11 14:58:53 +0200

    Adds proper enum

--- webapp/src/main/java/webapp/resolver/BookAssetsResolver.java
+++ webapp/src/main/java/webapp/resolver/BookAssetsResolver.java
@@ -18,7 +18,10 @@ public class BookAssetsResolver {
     @SchemaMapping(typeName = "Book", field = FIELD_NAME)
     public List<Asset> assets(Book book) {
         //TODO extract a proper collaborator or service to fetch assets
-        return List.of(new Asset("image", "http://example.com"), new Asset("video", "http://example2.com"));
+        return List.of(
+            new Asset("IMAGE", "http://example.com"),
+            new Asset("VIDEO", "http://example2.com")
+        );
     }
 }
 
